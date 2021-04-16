db0e1c890d902cbb8499f23bd7f155b7f7b6a72e
SobiaF <sobiafurqan@hotmail.com>
2021-04-16 20:02:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Hi there I will be using my powers to " +
+    "generator a number of the day specially just for you")
+let faveColor = prompt("What is your fave color?");
+alert("Cool so your fave color is " + faveColor);
+let faveCity = prompt("What is your fave city?");
+alert("So your fave city is " + faveCity);
+alert("Based on this information somehow " +
+    "I will give you your number of the day 👀");
+let numberOfDay = Math.round(Math.random() * 346);
+alert("You number of the day is " + numberOfDay);
